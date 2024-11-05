04719df9ff7a6c3ed04a0813c8a51bc9dee348ef
wesleydewinne <wesleydewinne@gmail.com>
2024-11-05 17:14:17 +0100

    huiwerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
+import {bestSellingTv} from "./constants/inventory.js";
 
 function App() {
   return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
+      <>
+        <h1>Begin hier met met maken van de applicatie!</h1>
+        {bestSellingTv.type}
+        {amountSold()}
+      </>
+
   )
 }
 
---
+++ src/helpers/amountSold.js
@@ -0,0 +1,8 @@
+ function amountSold () {
+    for (let i = 0; i < tvArray.length; i++) {
+
+    }
+
+ }
+
+ export default amountSold;
